8e4272c78bf93a9b9fe0e28735a529c772a52c6e
unknown <jejosu346@gmail.com>
2024-06-04 21:49:14 -0300

    estrutura basica das pastas e separacao da chamada da PAI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { useState } from "react"
+import { GetUsersList } from "./assets/api/Api";
+
+export interface UserType {
+  id: number, 
+  name: string, 
+  email: string, 
+  address: {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: {
+      lat: string,
+      lng: string
+    }
+  }
+}
+
 function App() {
+
+  const [ users, setUsers ] = useState<UserType>();
+
+  let teste = "";
+
+  if(!users)
+    teste = GetUsersList();
 
   return (
     <>
-      "HELLO"
+      {teste}
     </>
   )
 }
---
+++ src/assets/api/Api.ts
@@ -0,0 +1,26 @@
+export function GetUsersList () {
+
+  // https://jsonplaceholder.typicode.com/users
+
+
+  // return "hello";
+
+  // fetch("flowers.jpg")
+  // .then(function (response) {
+  //   if (response.ok) {
+  //     response.blob().then(function (myBlob) {
+  //       var objectURL = URL.createObjectURL(myBlob);
+  //       myImage.src = objectURL;
+  //     });
+  //   } else {
+  //     console.log("Network response was not ok.");
+  //   }
+  // })
+  // .catch(function (error) {
+  //   console.log(
+  //     "There has been a problem with your fetch operation: " + error.message,
+  //   );
+  // });
+
+  return "hello";
+}
