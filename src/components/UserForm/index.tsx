72eb59b9c1cc1089b04f282c9c87d3a0e2f25f4a
unknown <jejosu346@gmail.com>
2024-06-10 13:13:42 -0300

    integrando formulario de usuario

--- src/components/UserForm/index.tsx
+++ src/components/UserForm/index.tsx
@@ -1,20 +1,50 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UsersContext } from "../../App";
 
 import "./index.css";
+import { UserType } from "../../types/User";
 
 export function UserForm() {
 
   const { setUsers } = useContext(UsersContext);
 
-  const handleSubmit = () => (console.log("handler"));
+  const [ name, setName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ lat, setLat ] = useState("");
+  const [ lng, setLng ] = useState("");
+  
+  const user: UserType = {
+    id: 0, 
+    name: name, 
+    email: email, 
+    address: {
+      street: "",
+      suite: "",
+      city: "",
+      zipcode: "",
+      geo: {
+        lat: lat,
+        lng: lng
+      }
+    }
+  }
+
+  const handleSubmit = () => {
+    setUsers(prevUsers => [...prevUsers, user]);
+  };
+
+
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value); 
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value); 
+  const handleLat = (e: React.ChangeEvent<HTMLInputElement>) => setLat(e.target.value); 
+  const handleLng = (e: React.ChangeEvent<HTMLInputElement>) => setLng(e.target.value); 
 
   return (
     <div className="form">
-      <input type="text" placeholder="Nome"/>
-      <input type="email" placeholder="Email"/>
-      <input type="text" placeholder="Latitude"/>
-      <input type="text" placeholder="Longitude"/>
+      <input type="text" placeholder="Nome" value={user.name} onChange={handleName}/>
+      <input type="email" placeholder="Email" value={user.email} onChange={handleEmail}/>
+      <input type="text" placeholder="Latitude" value={user.address.geo.lat} onChange={handleLat}/>
+      <input type="text" placeholder="Longitude" value={user.address.geo.lng} onChange={handleLng}/>
       <button onClick={handleSubmit}>Adicionar</button>
     </div>
   );
